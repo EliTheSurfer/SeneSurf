3461627e0fa8d222421ce18aa970fc9457840a72
Eli Sall <elimane.sall@telecomnancy.net>
2019-04-02 16:12:44 +0200

    Refactor navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { CardComponent } from './card/card.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      CardComponent
+      CardComponent,
+      NavbarComponent
    ],
    imports: [
       BrowserModule,
