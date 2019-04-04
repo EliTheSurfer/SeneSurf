34c7bec40df1205f7190dbc8d4cbd1e4c59b7712
Eli Sall <elimane.sall@telecomnancy.net>
2019-04-04 16:23:07 +0200

    Refactoring meteoCards

--- src/app/meteoCard/meteoCard.component.ts
+++ src/app/meteoCard/meteoCard.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-meteoCard',
@@ -7,7 +9,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MeteoCardComponent implements OnInit {
 
-  constructor() { }
+  @Input() foreCastUrl = '//fr.surf-forecast.com/breaks/Le-Virage/forecasts/widget/a | safe';
+  constructor(public sanitizer: DomSanitizer) { }
 
   ngOnInit() {
   }
